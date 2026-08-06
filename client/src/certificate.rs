2c320a7e4ba6ac880c9d4ea9f7f7eb4d4c0ab764
pavankalyan0424 <addepalli.005@gmail.com>
2026-08-06 23:19:40 +0530

    Attestation Client updates: Pass APP_CERT_ALT_NAMES as SAN extension in CSR

--- client/src/certificate.rs
+++ client/src/certificate.rs
@@ -61,18 +61,19 @@ impl AppCert {
 
     pub(crate) fn request_app_cert_csr(&mut self, attributes: Vec<Attribute>) -> Result<String> {
         // Obtain domain names requested by the user
-        let alt_names = Self::get_alt_names().unwrap_or_default();
-
-        let subject = NameBuilder::new()
-            .add_common_name_iter(alt_names)
-            .build_subject();
+        let alt_names = Self::get_alt_names();
+        let subject = match alt_names.clone().as_ref().and_then(|names| names.first()) {
+            Some(domain) => NameBuilder::new().add_common_name(domain).build_name(),
+            None => vec![].into(),
+        };
 
         // Include [attestation_certificate, node_certificate] as extension in the body
         let pk_key = &mut self.key;
         let csr = Csr::mbedtls_crypto_builder()
             .with_self_signing_key(pk_key, pkix::types::RsaPkcs15(pkix::types::Sha256))
             .map_err(|e| CryptoErr(format!("failed to create crypto csr builder :{:?}", e)))?
-            .with_subject(subject)
+            .with_subject(subject.into())
+            .with_san_extension_from_strs(&alt_names.unwrap_or_default())
             .with_attributes(attributes)
             .map_err(|e| AppCertErr(format!("failed to add csr attributes : {:?}", e)))?
             .build_csr()
